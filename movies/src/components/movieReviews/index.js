49ef0b45410742a79f419a5c9896a9c9e91dd693
unknown <jiahchengpan@gmail.com>
2024-10-25 13:36:43 +0100

    Updating movie page to use react query

--- movies/src/components/movieReviews/index.js
+++ movies/src/components/movieReviews/index.js
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import Spinner from '../spinner'
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
@@ -11,13 +13,20 @@ import { getMovieReviews } from "../../api/tmdb-api";
 import { excerpt } from "../../util";
 
 export default function MovieReviews({ movie }) {
-    const [reviews, setReviews] = useState([]);
+    const { data, error, isLoading, isError } = useQuery(
+        ["reviews", { id: movie.id }],
+        getMovieReviews
+    );
 
-    useEffect(() => {
-        getMovieReviews(movie.id).then((reviews) => {
-            setReviews(reviews);
-        });
-    }, []);
+    if (isLoading) {
+        return <Spinner />;
+    }
+
+    if (isError) {
+        return <h1>{error.message}</h1>;
+    }
+
+    const reviews = data.results;
 
     return (
         <TableContainer component={Paper}>
